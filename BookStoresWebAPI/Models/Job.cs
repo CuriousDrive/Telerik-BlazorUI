a7dca33673b15d359f543a75d69cc3afa631c2bd
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-06-19 07:38:05 -0400

    Restructuring the folders

---
+++ BookStoresWebAPI/Models/Job.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStoresWebAPI.Models
+{
+    public partial class Job
+    {
+        public short JobId { get; set; }
+        public string JobDesc { get; set; }
+    }
+}
---
+++ Episode 1/BookStores/Client/Models/Author.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Models
+{
+    public class Author
+    {   
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }        
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+        public int Salary { get; set; }
+        public string Phone { get; set; }
+        
+        public Author()
+        {
+
+        }
+        public Author(int authorId, string firstName, string lastName, string emailAddress,int salary,
+                    string phoneNumber, string city)
+        {
+            AuthorId = authorId;
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            EmailAddress = emailAddress;
+            Phone = phoneNumber;
+            City = city;
+        }
+
+        public void clear()
+        {
+            AuthorId = 0;
+            FirstName = "";
+            LastName = "";
+            Salary = 0;
+            EmailAddress = "";
+            Phone = "";
+            City = "";
+        }
+    }
+}
