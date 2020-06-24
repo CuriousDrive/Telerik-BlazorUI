5210fce6c112bbdfbc309fcf27dd76f6a8c0a5d3
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-06-24 04:51:23 -0400

    Adding some code for Dropdownlist

---
+++ Episode 1/BookStores/Client/Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Models
+{
+    public class City
+    {
+        public string Id { get; set; }
+        public string CityName { get; set; }
+    }
+}
