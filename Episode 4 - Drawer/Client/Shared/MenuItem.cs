37937eee76a44fa053491d6dd8a93700e579c9b5
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-09-07 08:36:31 -0400

    exploring telerik drawer - day 1

---
+++ Episode 4 - Drawer/Client/Shared/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Client.Shared
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Icon { get; set; }
+        public string Url { get; set; }
+    }
+}
