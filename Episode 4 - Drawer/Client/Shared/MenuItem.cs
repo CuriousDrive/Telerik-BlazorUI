075535c71af6f6ea16cd2d4af35fc12e8e66f117
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-09-10 08:33:37 -0400

    Starting point for section - 3

---
+++ Episode 4 - Drawer/Client/Shared/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Client.Shared
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
+}
