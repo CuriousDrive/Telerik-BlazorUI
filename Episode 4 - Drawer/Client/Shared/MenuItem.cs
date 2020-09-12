434c3c64a8232d10a42d59f4c449d0b39430a6b6
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-09-12 16:40:56 -0400

    Done with section - 3

--- Episode 4 - Drawer/Client/Shared/MenuItem.cs
+++ Episode 4 - Drawer/Client/Shared/MenuItem.cs
@@ -9,5 +9,8 @@ namespace BookStores.Client.Shared
     {
         public string Text { get; set; }
         public string Url { get; set; }
+        public string Icon { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsSeparator { get; set; }
     }
 }
