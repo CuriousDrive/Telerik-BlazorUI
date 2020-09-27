b39928321055db3120fb186f4c7449bc1f675c59
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-09-27 08:20:26 -0400

    Creating a folder for document processing

---
+++ Episode 5 - Document Processing/Client/Shared/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Client.Shared
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+        public string ImageUrl { get; set; }
+        public string Group { get; set; }
+        public bool IsSeparator { get; set; }
+    }
+}
