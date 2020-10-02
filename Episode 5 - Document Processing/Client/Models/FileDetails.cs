fd66b949083981ce4ceb8bf8ff8039fd8be201a6
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-10-02 16:41:31 -0400

    Getting ready for document processing video

---
+++ Episode 5 - Document Processing/Client/Models/FileDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Client.Models
+{
+    public class FileDetails
+    {
+        public string Name { get; set; } = string.Empty;
+        public byte[] Data { get; set; }
+    }
+}
