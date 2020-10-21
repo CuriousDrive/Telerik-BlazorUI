e8b332c1f9c37842cff2bf1402fecfe631fa3b95
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-10-21 14:56:47 -0400

    Done with all the document processing examples

---
+++ Episode 5 - Document Processing/Client/Models/DocumentProcessing/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Client.Models.DocumentProcessing
+{
+    public class FileHelper
+    {
+        public int ID { get; set; }
+
+        public int CompressedSize { get; set; }
+
+        public int UncompressedSize { get; set; }
+
+        public string FileNameInZip { get; set; }
+
+        public string IconPath { get; set; }
+
+        public byte[] Data { get; set; }
+    }
+}
