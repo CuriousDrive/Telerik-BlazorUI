64dc9216f9b08254a2d2a42947523f41b1221b8c
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-11-01 12:53:19 -0500

    Added export data to excel section

---
+++ Episode 5 - Document Processing/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentProcessing.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int BooksCount { get; set; }
+    }
+}
---
+++ Episode 5 - Document Processing/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentProcessing.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int Sales { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
