d947d4d23ed23293c5ae7e74a496e157485c29b7
Curious Drive <CuriousDrive@users.noreply.github.com>
2020-11-07 07:09:47 -0500

    Done with telerik videos :)

--- Episode 5 - Document Processing/Utilities/FileDetails.cs
+++ Episode 5 - Document Processing/Utilities/FileDetails.cs
@@ -7,6 +7,11 @@ namespace DocumentProcessing.Utilities
 {
     public class FileDetails
     {
+        public int ID { get; set; }
+        public int CompressedSize { get; set; }
+        public int UncompressedSize { get; set; }
+        public string FileNameInZip { get; set; }
+        public string IconPath { get; set; }
         public string Name { get; set; } = string.Empty;
         public byte[] Data { get; set; }
     }
